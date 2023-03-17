44c2a6f948f633fd9048034f46283731b0385b42
Olga Tolstykhina <ovt0709@gmail.com>
2023-03-17 22:28:23 +0200

    handlebars use

--- src/markupcoutrylist.js
+++ src/markupcoutrylist.js
@@ -1,12 +1,6 @@
+import markUpTpl from './markupcountrylist.hbs'
+
 export default function countryListTpl(country) {
-  return country
-    .map(
-      ({ name: { official }, flags: { svg } }) => `
-      <li class="country-list__item">
-        <img class="country__flag" src="${svg}" alt="flag of ${official}" width="35" height="20">
-        <p class="country__name">${official}</p>
-      </li>
-    `
-    )
-    .join("");
+  return country.map(({ name: { official }, flags: { svg } }) => markUpTpl({ official, svg })).join("");
 }
+
